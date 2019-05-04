c5ce28cd96869ddfadab2728b24bd1d1931a6155
marina-saad <32791749+marina-saad@users.noreply.github.com>
2019-05-04 16:20:00 +0200

    get contours

--- app/src/main/java/com/example/cardscanner/ImgProcPipeline.java
+++ app/src/main/java/com/example/cardscanner/ImgProcPipeline.java
@@ -123,5 +123,196 @@ public class ImgProcPipeline {
         faceImgOnly.copyTo(dst);
 
     }
+    
+        private static void resizeHeight(Mat src, Mat dst, double height) {
+        int w = src.width();
+        int h = src.height();
+        double r = height / (float) (h);
+        Size dim = new Size((int) (w * r), height);
+        Imgproc.resize(src, dst, dim, Imgproc.INTER_AREA);
+    }
+
+
+    private static Pair<Point, Integer> getMinX(ArrayList<Point> Points) {
+        Point tempX = Points.get(0).clone();
+        int index = 0;
+        for (int i = 0; i < Points.toArray().length; i++) {
+            if (Points.get(i).x < tempX.x) {
+                tempX.x = Points.get(i).x;
+                tempX.y = Points.get(i).y;
+                index = i;
+            }
+        }
+        return new Pair<>(tempX, index);
+    }
+
+    private static double getMaxPerimeter(List<MatOfPoint> contours) {
+        double maxPerimeter = Imgproc.arcLength(new MatOfPoint2f(contours.get(0).toArray()), true);
+        if (contours.toArray().length > 1) {
+            for (MatOfPoint c : contours) {
+                double perimeter = Imgproc.arcLength(new MatOfPoint2f(c.toArray()), true);
+                if (perimeter > maxPerimeter) {
+                    maxPerimeter = perimeter;
+                }
+            }
+        }
+        return maxPerimeter;
+    }
+
+
+    private static MatOfPoint2f getFourBoundaryPts(Mat input_img, double NEW_HEIGHT) {
+
+
+        //Resize the img to be able to find the contours
+        double ratio = input_img.height() / NEW_HEIGHT;
+        resizeHeight(input_img, input_img, NEW_HEIGHT);
+
+        Mat gray_img = new Mat();
+        Mat blurred_img = new Mat();
+        Mat edges_img = new Mat();
+
+        /*  EDGES IMAGE (White pixels --> card edges, Black pixels --> Otherwise) */
+        Imgproc.cvtColor(input_img, gray_img, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.GaussianBlur(gray_img, blurred_img, new Size(5, 5), 0);
+        Imgproc.Canny(blurred_img, edges_img, 70, 200);
+
+
+        /*  DEBUGGING to view the edges img bitmap  */
+        Bitmap edges = null;
+        edges = Bitmap.createBitmap(edges_img.width(), edges_img.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(edges_img, edges);
+        /*  DEBUGGING  */
+
+
+        /*   FINDING CONTOURS OF CARD   */
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(edges_img, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+        MatOfPoint2f cardContour = new MatOfPoint2f();
+
+
+        boolean cardContourFound = false; //flag
+        double maxPerimeter = getMaxPerimeter(contours);
+        int counter = 3; //try for 3 times only in order not to fall into an infinite loop
+        while (counter >= 0) {
+            for (MatOfPoint c : contours) {
+                double perimeter = Imgproc.arcLength(new MatOfPoint2f(c.toArray()), true);
+                MatOfPoint2f approximatePolygon = new MatOfPoint2f();
+                Imgproc.approxPolyDP(new MatOfPoint2f(c.toArray()), approximatePolygon, 0.02 * perimeter, true);
+
+                if (approximatePolygon.toArray().length == 4) {
+
+                    if (contours.toArray().length == 1) {
+
+                        cardContour = approximatePolygon;
+                        cardContourFound = true;
+                        counter = -1;
+                        break;
+
+                    } else {
+                        if (perimeter == maxPerimeter && maxPerimeter > 600) {
+                            cardContour = approximatePolygon;
+                            cardContourFound = true;
+                            counter = -1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!cardContourFound) {
+                //fill the card's segments
+                Mat filledEdges = new Mat();
+                for (int i = 0; i < contours.toArray().length; i++) {
+                    Imgproc.drawContours(edges_img, contours, i, new Scalar(255, 255, 255), Imgproc.FILLED);
+                }
+
+                //apply morphological close operation to let it have one contour only
+                Mat Kernel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(3, 3));
+                Imgproc.morphologyEx(edges_img, filledEdges, Imgproc.MORPH_CLOSE, Kernel, new Point(-1, -1), 10);
+
+
+                //find contours again after the close operation
+                contours = new ArrayList<>();
+                hierarchy = new Mat();
+                Imgproc.findContours(filledEdges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                maxPerimeter = getMaxPerimeter(contours);
+                counter--;
+            }
+
+        }
+
+
+        if (cardContour.empty()) {
+            return null;
+        }
+
+        ArrayList<Point> resizedPoints = new ArrayList<>();
+        for (Point P : cardContour.toArray()) {
+            Point temp = new Point();
+            temp.x = P.x * ratio;
+            temp.y = P.y * ratio;
+
+            resizedPoints.add(temp);
+        }
+
+        /*  REARRANGING THE CORNER POINTS FOUND --> to be suitable for the next method  */
+        Point tempMinX1 = new Point();
+        int idx1 = 0;
+        Pair<Point, Integer> minX1;
+        minX1 = getMinX(resizedPoints);
+
+        tempMinX1 = minX1.first.clone();
+        idx1 = minX1.second;
+        resizedPoints.remove(idx1);
+
+
+        Point tempMinX2 = new Point();
+        int idx2 = 0;
+        Pair<Point, Integer> minX2;
+        minX2 = getMinX(resizedPoints);
+
+        tempMinX2 = minX2.first.clone();
+        idx2 = minX2.second;
+        resizedPoints.remove(idx2);
+
+
+        Point tl, bl, tr, br;
+        if (tempMinX1.y > tempMinX2.y) {
+            bl = tempMinX1;
+            tl = tempMinX2;
+        } else {
+            tl = tempMinX1;
+            bl = tempMinX2;
+        }
+
+        if (resizedPoints.get(0).y > resizedPoints.get(1).y) {
+            br = resizedPoints.get(0);
+            tr = resizedPoints.get(1);
+        } else {
+            tr = resizedPoints.get(0);
+            br = resizedPoints.get(1);
+        }
+
+        /*  The order accepted by the transformation fn is as follows: { tl, bl, br, tr }  */
+        ArrayList<Point> resizedPointsNew = new ArrayList<>();
+        resizedPointsNew.add(tl);
+        resizedPointsNew.add(bl);
+        resizedPointsNew.add(br);
+        resizedPointsNew.add(tr);
+
+
+        MatOfPoint2f result = new MatOfPoint2f();
+        result.fromList(resizedPointsNew);
+
+        if (result.size() == new Size(0, 0)) {
+            Log.e(TAG, "The MatOfPoint2f doesn't contain the pts.");
+        }
+
+        return result;
+    }
 
 }
